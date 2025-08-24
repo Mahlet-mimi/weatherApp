3470dd5b7474fb844641f151737a9d20f3a8615f
Mahlet-mimi <mimisha1513@gmail.com>
2025-08-24 14:09:23 -0700

    Updated the link of URL

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  console.log(response.data)
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
